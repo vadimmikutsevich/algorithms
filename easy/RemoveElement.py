3af2eaf23728188630bb28b966446a2635f3d064
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2022-11-30 18:25:04 +0400

    add(easy): remove element

---
+++ easy/RemoveElement.py
@@ -0,0 +1,16 @@
+def removeElement(nums, val):
+    k = 0
+    n = len(nums)
+
+    while k < n:
+        if nums[k] == val:
+            nums[k] = nums[n - 1]
+            n -= 1
+        else:
+            k += 1
+
+    return k
+
+removeElement([5,2,4,7,1,4,7,8,3], 4)
+
+# we are expect [5, 2, 3, 7, 1, 8, 7, 8, 3]
