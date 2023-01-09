586b07ea3d02048f3b867e211059e8bbc4e83ec4
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2023-01-09 18:11:59 +0400

    MiddleOfTheLinkedList

---
+++ MiddleOfTheLinkedList.py
@@ -0,0 +1,35 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+one = ListNode(1)
+two = ListNode(2)
+three = ListNode(3)
+four = ListNode(4)
+five = ListNode(5)
+
+one.next = two
+two.next = three
+three.next = four
+four.next = five
+
+head = one
+
+
+def middleNode(head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+    
+print(middleNode(head))
+
+
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
